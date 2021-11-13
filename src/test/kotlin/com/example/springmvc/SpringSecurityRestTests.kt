77d248770eeb44c9d93605f4cf4c2515931e67b7
Дмитрий l W.W <shigalkin@mail.ru>
2021-11-13 14:05:13 +0300

    tests is done

--- src/test/kotlin/com/example/springmvc/SpringSecurityRestTests.kt
+++ src/test/kotlin/com/example/springmvc/SpringSecurityRestTests.kt
@@ -3,8 +3,9 @@ package com.example.springmvc
 
 import com.example.springmvc.DAO.BookNote
 import com.example.springmvc.service.AddressBookService
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -19,16 +20,14 @@ import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 import org.springframework.web.context.WebApplicationContext
+import java.awt.print.Book
+import java.util.concurrent.ConcurrentHashMap
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SpringSecurityRestTests {
+class SpringSecurityRestTests(@LocalServerPort var port: Int) {
 
-    @LocalServerPort
-    private var port: Int = 0
-
-    @Autowired
-    private val context: WebApplicationContext? = null
+    private val headers = HttpHeaders()
 
     @Autowired
     private lateinit var addressBookService: AddressBookService
@@ -36,70 +35,125 @@ class SpringSecurityRestTests {
     @Autowired
     private lateinit var restTemplate: TestRestTemplate
 
+    private val fullURI = "http://localhost:$port/"
+
+    private fun getCookie(
+        username: String,
+        password: String,
+        loginURI: String = "$fullURI/login"
+    ): String {
+
+        val form = LinkedMultiValueMap<String, String>()
+        form.set("username", username)
+        form.set("password", password)
+
+        val loginResponse = restTemplate
+            .postForEntity(
+                loginURI,
+                HttpEntity(form, HttpHeaders()),
+                String::class.java
+            )
+
+        return loginResponse.headers["Set-Cookie"]!![0]
+    }
+
     @BeforeEach
     fun setup() {
+        val cookie = getCookie("john", "john")
+        headers.add("Cookie", cookie)
 
         addressBookService.addNote(BookNote("Michail", "Ivanov", "Lenina Street", "+78756782233"))
         addressBookService.addNote(BookNote("Dmitry", "Zhigalkin", "Lenina Street", "none"))
         addressBookService.addNote(BookNote("Artem", "Afimin", "Amyrskaya Street", "+228"))
     }
 
-    @Test
-    fun `should return list`() {
-        val result: ResponseEntity<String> = restTemplate.withBasicAuth("john", "john")
-            .getForEntity("/api/list", String::class.java)
+    @ParameterizedTest
+    @MethodSource("different notes")
+    fun `should add address with authorities = API_ROLE`(note: BookNote) {
+        val URL = "$fullURI/api/add"
 
-        assertEquals(HttpStatus.OK, result.statusCode)
-        Assertions.assertNotNull(result.body)
+        val response = restTemplate
+            .exchange(
+                URL,
+                HttpMethod.POST,
+                HttpEntity(note, headers),
+                BookNote::class.java)
+
+        assertEquals(HttpStatus.CREATED, response.statusCode)
+        assertTrue(response.body!! == note)
+    }
+
+    @Test
+    fun `should get list of addresses`() {
+        val map = emptyMap<String, String>()
+
+        val URL = "$fullURI/api/list"
+
+        val response = restTemplate.exchange(
+            URL, HttpMethod.POST,
+            HttpEntity(map, headers),
+            Map::class.java)
+
+        assertEquals(response.statusCode, HttpStatus.OK)
+        assertNotNull(response.body)
+    }
+
+    @ParameterizedTest
+    @MethodSource("different notes")
+    fun `should update address`(note: BookNote) {
+        val URL = "$fullURI/api/0/edit"
+
+        val response = restTemplate.exchange(
+            URL,
+            HttpMethod.PUT,
+            HttpEntity(note, headers),
+            BookNote::class.java, 1)
+
+        assertNotNull(response.body)
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(note.name, response.body!!.name)
+        assertEquals(note.surname, response.body!!.surname)
+        assertEquals(note.address, response.body!!.address)
+        assertEquals(note.telephone, response.body!!.telephone)
+    }
+
+    @ParameterizedTest
+    @MethodSource("different id")
+    fun `should delete address`(id: Int) {
+        val URL = "$fullURI/api/$id/delete"
+
+        val response = restTemplate.exchange(
+            URL,
+            HttpMethod.DELETE,
+            HttpEntity(null, headers),
+            BookNote::class.java)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
     }
 
     @Test
     fun `should return book with id = 0`() {
-        val result: ResponseEntity<String> = restTemplate.withBasicAuth("john", "john")
-            .getForEntity("/api/0/view", String::class.java)
+        val URL = "$fullURI/api/0/view"
 
-        assertEquals(HttpStatus.OK, result.statusCode)
-        Assertions.assertNotNull(result.body)
+        val response = restTemplate.exchange(
+            URL,
+            HttpMethod.GET,
+            HttpEntity(null, headers),
+            ConcurrentHashMap::class.java)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
     }
 
-//    @Test
-//    fun `should return list1`() {
-//        logging()
-//        val bookNote = BookNote("Maxom", "Kozlov", "Porty Street", "71018")
-//
-//        val response = restTemplate.exchange(
-//            url("api/add"),
-//            HttpMethod.POST,
-//            HttpEntity(bookNote),
-//            BookNote::class.java
-//        )
-//
-//        assertEquals(HttpStatus.CREATED, response.statusCode)
-//    }
-
-//    @Test
-//    fun `should return list1`() {
-//        val a = BookNote("Maxom", "Kozlov", "Porty Street", "71018")
-//
-//        val result: ResponseEntity<String> = restTemplate.withBasicAuth("john", "john")
-//            .postForEntity("/api/add", HttpEntity(a), String::class.java)
-//
-//        assertEquals(HttpStatus.CREATED, result.statusCode)
-//    }
-
-//    @ParameterizedTest
-//    @MethodSource("different book note")
-//    fun `should correct add notes`(bookNote: BookNote) {
-//        val response = restTemplate.exchange(
-//            url("api/add"),
-//            HttpMethod.POST,
-//            HttpEntity(bookNote, headers),
-//            BookNote::class.java
-//        )
-//
-//        assertEquals(HttpStatus.CREATED, response.statusCode)
-//        Assertions.assertNotNull(response.body)
-//        assertEquals(bookNote.name, response.body!!.name)
-//    }
-
+    companion object {
+        @JvmStatic
+        fun `different notes`() = listOf(
+            BookNote("Ivan", "Ivanov", "Lenina Street", "+1"),
+            BookNote("Dmitry", "Petrov", "Omskaya Street", "+72022"),
+            BookNote("artem", "nayd", "1", "228")
+        )
+        @JvmStatic
+        fun `different id`() = listOf(
+            2, 0, 1
+        )
+    }
 }
